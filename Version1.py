4a0e1dd48567ed403de2267fa6ac078d82e7a63d
Soh9l <sohequre@gmail.com>
2026-08-08 01:49:34 +0530

    Task 3 Options A

--- Version1.py
+++ Version1.py
@@ -1,15 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask("MyAssignment")
+
+voters_list = [{"person":"No One","Vote":0}]
 
 
 @app.route("/")
 def index():
-    return "Welcome to the App"
+    return f"""Welcome to the App
+            Use the url to register your vote by entering the url as /vote/'type in your name'"""
 
 
 @app.route("/health")
 def helathstat():
     return "App is running"
 
+
+@app.get("/vote/<Name>")
+def voter_get(Name):
+    n = Name
+    for i in voters_list:
+        if i["person"] == n:
+           i["Vote"]+=1
+           return "Your vote is incremented"
+    new_voter={"person":n,"Vote":1}
+    voters_list.append(new_voter)
+    return "Vote registered"
+
+
+@app.get("/results")
+def results():
+    return jsonify(voters_list)
 
 app.run(debug=True)
