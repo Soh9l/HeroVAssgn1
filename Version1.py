57c3b36022220e7ca83d8b86d0a4d4a95e8e3655
Soh9l <sohequre@gmail.com>
2026-08-08 01:44:58 +0530

    Task 1

--- Version1.py
+++ Version1.py
@@ -1,34 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 app = Flask("MyAssignment")
-
-voters_list = [{"person":"No One","Vote":0}]
 
 
 @app.route("/")
 def index():
-    return f"""Welcome to the App
-            Use the url to register your vote by entering the url as /vote/'type in your name'"""
+    return "Welcome to the App"
 
 
 @app.route("/health")
 def helathstat():
     return "App is running"
 
-
-@app.get("/vote/<Name>")
-def voter_get(Name):
-    n = Name
-    for i in voters_list:
-        if i["person"] == n:
-           i["Vote"]+=1
-           return "Your vote is incremented"
-    new_voter={"person":n,"Vote":1}
-    voters_list.append(new_voter)
-    return "Vote registered"
-
-
-@app.get("/results")
-def results():
-    return jsonify(voters_list)
 
 app.run(debug=True)
